9dcccf58a341b63196da1e0dbb3fba75df23f0ee
brianLeeson <brianeleeson@gmail.com>
2016-09-30 13:54:36 -0700

    better checking of file type

--- pageserver.py
+++ pageserver.py
@@ -103,7 +103,7 @@ def valid(path):
     file_type = path.split('.')[-1]
     print(file_type)
     print(len(file_type))
-    suffix = "html" in file_type or "css" in file_type
+    suffix = "html" is file_type or "css" is file_type
     print(suffix)
     return forbidden_text and suffix
 
